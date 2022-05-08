e048bbde161d1628cb1cb916bd572d589b03f4bf
vsuyazov <suiazov.vlad@gmail.com>
2022-05-08 13:38:00 +0300

    Update registration

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,13 +1,12 @@
 import java.util.*;
-import java.util.stream.Stream;
 
 public class StreamMain {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = intList.stream();
-        stream = stream.filter(x -> x > 0);
-        stream = stream.filter(x -> x % 2 == 0);
-        stream = stream.sorted(Comparator.naturalOrder());
-        stream.forEach(System.out::println);
+        intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
 }
